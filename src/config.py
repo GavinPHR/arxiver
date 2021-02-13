f585ece177524ac2abac55ab98aa25cd3cd4fd84
benmanson <benjmanson87@gmail.com>
2021-02-13 15:48:19 +0000

    Experiemnt with different stemmers

--- src/config.py
+++ src/config.py
@@ -1,22 +1,29 @@
+import nltk
+from stemming.porter2 import stem
+
 # global vars
 STEMMING = True
 STOPPING = False
 JSON_PATH = "../arxiv_sampled.json"
 STOP_PATH = "../englishST.txt"
 
+if STEMMING:
+    porter = nltk.stem.porter.PorterStemmer()
+    sno = nltk.stem.snowball.SnowballStemmer("english")
+
 def get_stop_words():
-    lines = open(STOP_PATH, 'r', encoding='utf-8-sig').readlines()
+    lines = open(STOP_PATH, "r", encoding="utf-8-sig").readlines()
 
     # create list of words to return
     words = []
     for line in lines:
-        words.extend(re.split('[^\w\']', line))
+        words.extend(re.split("[^\w\']", line))
 
-    # stem words and/or remove empty strings, convert 'term' to term
+    # stem words and/or remove empty strings, convert "term" to term
     if STEMMING:
-        stops = set([stem(re.sub('\'$', '', re.sub('^\'', '', word))) for word in words if word != ''])
+        stops = set([stem(re.sub("\'$", "", re.sub("^\'", "", word))) for word in words if word != ""])
         return stops
     
-    return set([re.sub('\'$', '', re.sub('^\'', '', word)) for word in words if word != ''])
+    return set([re.sub("\'$", "", re.sub("^\'", "", word)) for word in words if word != ""])
 
 STOP_WORDS = get_stop_words() + "" if STOPPING else set("")
