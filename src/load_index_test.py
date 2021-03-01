475477a9340ca5a9150e4a3ef3a281451860e8ca
benmanson <benjmanson87@gmail.com>
2021-03-01 16:11:34 +0000

    Time load

--- src/load_index_test.py
+++ src/load_index_test.py
@@ -1,7 +1,11 @@
 import utils
+import time
 
 if __name__=="__main__":
-    index = utils.load_index()
+    start_time = time.time()
+    index = utils.load_index(filename="indexes/inverted_index_t")
+    end_time = time.time()
+    print("Took {} seconds to load".format(end_time-start_time))
 
     while True:
         print("Enter a word to search the index for:")
