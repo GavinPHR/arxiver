bbf21319dc2c02273125a49881e0c4135ec66a69
benmanson <benjmanson87@gmail.com>
2021-03-06 00:06:00 +0000

    Change load index search method

--- src/load_index_test.py
+++ src/load_index_test.py
@@ -1,17 +1,25 @@
 from src import utils
 import time
+import os
+from src.config import *
 
 if __name__=="__main__":
     while True:
         print("Enter a word to search the index for:")
         x = input()
 
-        start_time = time.time()
-        index = utils.load_index(filename="indexes/inverted_index_" + x[0])
-        end_time = time.time()
-        print("Took {} seconds to load".format(end_time-start_time))
+        if x != "":
+            start_time = time.time()
+            if os.path.exists(("indexes/inverted_index_" + x + ".pbz2")):
+                index = utils.load_index("indexes/inverted_index_" + x)
+                loaded = x
+            else:
+                index = utils.load_index(filename="indexes/inverted_index_" + x[0])
+                loaded = x[0]
+            end_time = time.time()
+            print(("Took {} seconds to load index " + loaded).format(end_time-start_time))
 
-        try:
-            print(index[x])
-        except KeyError:
-            print("Sorry, this word is not in the index. Try another.")
+            try:
+                print(index[x]["doc_frequency"])
+            except KeyError:
+                print("Sorry, this word is not in the index. Try another.")
