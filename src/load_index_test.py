d9a6c853a77ef37684599f6efb86d1014829afbd
benmanson <benjmanson87@gmail.com>
2021-03-11 11:54:32 +0000

    Add citations counts

--- src/load_index_test.py
+++ src/load_index_test.py
@@ -5,7 +5,8 @@ from src.config import *
 from tqdm import tqdm
 
 if __name__=="__main__":
-    vocab = utils.get_vocabulary()
+    vocab = utils.get_vocabulary()          # loads vocabulary present in system
+    citations = utils.get_citations()       # loads citation counts for doc_ids
     while True:
         print("Enter a word to search the index for:")
         x = input()
@@ -22,5 +23,7 @@ if __name__=="__main__":
                 end_time = time.time()
                 print(("Took {} seconds to load index " + loaded).format(end_time-start_time))
                 print(index[x]["doc_frequency"])
+                for k in list(index[x]["doc_ids"])[:10]:
+                    print(k, citations[k])
             else:
                 print("Sorry, this word is not in the index. Try another.")
