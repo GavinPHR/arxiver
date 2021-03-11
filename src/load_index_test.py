2195adc48b846a0bd63b1da1749dbee9386e49bc
benmanson <benjmanson87@gmail.com>
2021-03-11 12:27:55 +0000

    Change printing

--- src/load_index_test.py
+++ src/load_index_test.py
@@ -11,19 +11,18 @@ if __name__=="__main__":
         print("Enter a word to search the index for:")
         x = input()
 
-        if x != "":
-            if x in vocab:
-                start_time = time.time()
-                if os.path.exists(("indexes/inverted_index_" + x + ".pbz2")):
-                    index = utils.load_index("indexes/inverted_index_" + x)
-                    loaded = x
-                else:
-                    index = utils.load_index(filename="indexes/inverted_index_" + x[0])
-                    loaded = x[0]
-                end_time = time.time()
-                print(("Took {} seconds to load index " + loaded).format(end_time-start_time))
-                print(index[x]["doc_frequency"])
-                for k in list(index[x]["doc_ids"])[:10]:
-                    print(k, citations[k])
+        if x in vocab:
+            start_time = time.time()
+            if os.path.exists(("indexes/inverted_index_" + x + ".pbz2")):
+                index = utils.load_index("indexes/inverted_index_" + x)
+                loaded = x
             else:
-                print("Sorry, this word is not in the index. Try another.")
+                index = utils.load_index(filename="indexes/inverted_index_" + x[0])
+                loaded = x[0]
+            end_time = time.time()
+            print(("Took {} seconds to load index " + loaded).format(end_time-start_time))
+            print(index[x]["doc_frequency"])                        # print number of docs term is in
+            for k in list(index[x]["doc_ids"].keys())[:10]:
+                print(k, index[x]["doc_ids"][k], citations[k])      # print top 10 docs for term, how many times term in doc, and citations of doc
+        else:
+            print("Sorry, this word is not in the index. Try another.")
