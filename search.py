b45eaffb96469cedcf54bd17f87d19d29a2b81ff
Ben Manson <45209132+benmanson@users.noreply.github.com>
2021-03-17 17:49:53 +0000

    Update search.py

--- search.py
+++ search.py
@@ -1,6 +1,7 @@
 import sys
 import re
 import pathlib
+import utils
 from config import *
 # the file which contains the index
 indexFile = str(pathlib.Path(__file__).parent.absolute()) + "/index.txt"
