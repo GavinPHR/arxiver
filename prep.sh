17164837e8da54df2e903260fd12a72b8046f96f
Ben Manson <45209132+benmanson@users.noreply.github.com>
2021-03-18 12:35:08 +0000

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -1,6 +1,6 @@
 DATA_DIR="/workspace/src/data"
 
 mkdir $DATA_DIR
-mv $DATA_DIR/../../ttds_data.tar $DATA_DIR
+mv $DATA_DIR/../ttds_data.tar $DATA_DIR
 tar -xvf $DATA_DIR/ttds_data.tar
 rm -rf $DATA_DIR/ttds_data.tar
