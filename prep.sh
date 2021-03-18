2af215a15536544df0db06a2a1f8e5b3dec33242
Ben Manson <45209132+benmanson@users.noreply.github.com>
2021-03-18 17:39:45 +0000

    Don't print tar output

--- prep.sh
+++ prep.sh
@@ -2,5 +2,5 @@ DATA_DIR="/workspace/src/data"
 
 mkdir $DATA_DIR
 mv $DATA_DIR/../ttds_data.tar $DATA_DIR
-tar -xvf $DATA_DIR/ttds_data.tar -C $DATA_DIR
+tar -xf $DATA_DIR/ttds_data.tar -C $DATA_DIR
 rm -rf $DATA_DIR/ttds_data.tar
