0dd0bf54ea5f61084b9b9a181d7f216cc1913721
Gavin Peng <gavinsweden@gmail.com>
2021-03-18 14:49:44 +0000

    id_list too long? truncate search result to length 500

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def retrieve(query):
 	# abstract = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non consectetur augue. Mauris mollis vitae tortor quis cursus. Vivamus imperdiet quis mauris ut posuere. Integer sit amet blandit erat, in congue dui. Etiam nec sagittis lorem. Vivamus in turpis faucibus, aliquet purus id, consectetur velit. Nulla tristique imperdiet nunc, at auctor enim mattis varius. Vivamus tristique, purus ut convallis aliquam, enim nulla sagittis nisl, eu accumsan risus arcu et velit. Quisque euismod fermentum est vel auctor. Nulla facilisi. Praesent fringilla, est ac porttitor volutpat, mauris tellus facilisis enim, eu aliquet dui urna a diam.'
 	results = []
 	print(query)
-	ids = search.searching(query['freetext'])
+	ids = search.searching(query['freetext'])[:500]
 	for i, file in enumerate(arxiv.query(id_list=ids)):
 		results.append({'link': 'https://arxiv.org/abs/' + ids[i],
 			            'title': file['title'], 
